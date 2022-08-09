115274b914a614f5c8d7ab7418ba881d4bf8e56d
J_look <9916320+J_look@user.noreply.gitee.com>
2022-08-09 16:16:09 +0800

    -- 1 配置mp逻辑删除

---
+++ src/main/java/look/word/reggie/entity/Category.java
@@ -0,0 +1,65 @@
+package look.word.reggie.entity;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
+/**
+ * 菜品及套餐分类(Category)表实体类
+ *
+ * @author LookWord
+ * @since 2022-08-09 15:22:04
+ */
+@Data
+@SuppressWarnings("serial")
+public class Category extends Model<Category> {
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 类型   1 菜品分类 2 套餐分类
+     */
+    private Integer type;
+    /**
+     * 分类名称
+     */
+    private String name;
+    /**
+     * 顺序
+     */
+    private Integer sort;
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+    /**
+     * 修改人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+    /**
+     * 逻辑删除 0 未删除 1 已删除
+     */
+    @TableField(value = "is_delete")
+    private Integer deleted;
+
+
+}
+
