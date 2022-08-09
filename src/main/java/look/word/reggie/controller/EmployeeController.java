abda55a22e0e829e0f6526aa9fe078e91a7815d2
J_look <9916320+J_look@user.noreply.gitee.com>
2022-08-09 10:27:33 +0800

    -- 1 全局异常处理器的添加

--- src/main/java/look/word/reggie/controller/EmployeeController.java
+++ src/main/java/look/word/reggie/controller/EmployeeController.java
@@ -7,15 +7,14 @@ import look.word.reggie.common.aop.LogAnnotation;
 import look.word.reggie.entity.Employee;
 import look.word.reggie.service.EmployeeService;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
+@RestControllerAdvice
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -73,5 +72,31 @@ public class EmployeeController {
         //清理Session中保存的当前登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+
+        //设置初始密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex(employee.getPhone().getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
--- src/main/java/look/word/reggie/entity/Employee.java
+++ src/main/java/look/word/reggie/entity/Employee.java
@@ -1,38 +1,67 @@
 package look.word.reggie.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import lombok.Data;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
+/**
+ * 员工信息(Employee)表实体类
+ *
+ * @author LookWord
+ * @since 2022-08-09 10:07:46
+ */
 @Data
-public class Employee implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+public class Employee extends Model<Employee> {
+    /**
+     * 主键
+     */
     private Long id;
-
-    private String username;
-
+    /**
+     * 姓名
+     */
     private String name;
-
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
     private String password;
-
+    /**
+     * 手机号
+     */
     private String phone;
-
+    /**
+     * 性别
+     */
     private String sex;
-
-    private String idNumber; //驼峰命名法 ---> 映射的字段名为 id_number
-
+    /**
+     * 身份证号
+     */
+    private String idNumber;
+    /**
+     * 状态 0:禁用，1:正常
+     */
     private Integer status;
-
+    /**
+     * 创建时间
+     */
     private LocalDateTime createTime;
-
+    /**
+     * 更新时间
+     */
     private LocalDateTime updateTime;
-
-    @TableField(fill = FieldFill.INSERT)
+    /**
+     * 创建人
+     */
     private Long createUser;
-
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    /**
+     * 修改人
+     */
     private Long updateUser;
+
+
 }
+
